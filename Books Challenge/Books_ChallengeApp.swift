90a77f757fe333298115545687a5b5fd2d2f23eb
rodriguesra <rodriguesra@hotmail.com>
2021-06-02 18:03:50 -0300

    Initial Commit

---
+++ Books Challenge/Books_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_ChallengeApp.swift
+//  Books Challenge
+//
+//  Created by Rafael Rodrigues on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Books_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Books Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Books Challenge
+//
+//  Created by Rafael Rodrigues on 02/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
